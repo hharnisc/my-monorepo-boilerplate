842b1b8d5da23e4a5cd83bbcd259cd09a36d01e8
Harrison Harnisch <hharnisc@gmail.com>
2018-01-17 08:49:39 -0600

    implement app-sidebar components

---
+++ packages/app-sidebar/components/AppSidebar/index.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import {
+  Link,
+  List,
+  ListItem,
+  Text,
+  Image,
+} from 'my-component-library-boilerplate';
+
+const menuItems = [
+  {
+    name: 'Packages',
+    href: '/packages',
+  },
+  {
+    name: 'Settings',
+    href: '/settings',
+  },
+];
+
+export default ({
+  profile,
+}) =>
+  <nav
+    style={{
+      height: '100%',
+      width: '100%',
+      padding: '1rem',
+      background: '#F2F2F2',
+      boxSizing: 'border-box',
+      display: 'flex',
+      flexDirection: 'column',
+    }}
+  >
+    <Text size={2}>My Monorepo App</Text>
+    <div
+      style={{
+        flexGrow: 1,
+      }}
+    >
+      <List
+        items={menuItems.map((item) =>
+          <Link
+            href={item.href}
+            unstyled
+          >
+            <div
+              style={{
+                paddingTop: '1rem',
+              }}
+            >
+              <Text size={1.5}>
+                {item.name}
+              </Text>
+            </div>
+          </Link>)}
+      />
+    </div>
+  <div
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+    }}
+  >
+  <Image
+    src={profile.image}
+    height={'2rem'}
+    width={'2rem'}
+  />
+    <div
+      style={{
+        flexGrow: 1,
+        paddingLeft: '1rem',
+      }}
+    >
+      <Text>{profile.name}</Text>
+    </div>
+  </div>
+  </nav>
---
+++ packages/app-sidebar/components/AppSidebar/story.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import AppSidebar from './index'
+
+storiesOf('AppSidebar', module)
+  .add('should render AppSidebar', () => (
+    // AppSidebar fills the parent container
+    // the parent container sets the size of the AppSidebar
+    <div
+      style={{
+        width: '300px',
+        height: '100%',
+      }}
+    >
+      <AppSidebar
+        profile={{
+          name: 'Bobson Dugnutt',
+          image: 'https://hharnisc.github.io/images/me3.jpg',
+        }}
+      />
+    </div>
+  ))
