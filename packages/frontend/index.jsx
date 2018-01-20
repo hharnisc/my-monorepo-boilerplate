ebbe25407cbd3cf47151a10f84bcdb8dd24d9b19
Harrison Harnisch <hharnisc@gmail.com>
2018-01-20 10:38:05 -0600

    hook up the store in the frontend package

--- packages/frontend/index.jsx
+++ packages/frontend/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import createStore from 'my-monorepo-boilerplate-store'
 import App from './components/App'
 
+// create the store and dispatch an APP_INIT action
+const store = createStore();
+store.dispatch({
+  type: 'APP_INIT',
+})
+
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 )
