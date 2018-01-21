6d28d169e88cc430f12a71d05b9d746f56ac8148
Harrison Harnisch <hharnisc@gmail.com>
2018-01-21 09:27:22 -0600

    implement user api endpoint

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -14,5 +14,13 @@ app.use(webpackMiddleware(compiler, {
 
 const html = readFileSync(join(__dirname, 'index.html'), 'utf8')
 
+app.get('/api/user', (req, res) => res.json({
+  user: {
+    profile: {
+      image: 'http://0.gravatar.com/avatar/cbb0e0bb8e68fe44de2dfde029aa0e06',
+      name: 'Bobson Dugnutt',
+    }
+  }
+}))
 app.get('*', (req, res) => res.send(html))
 app.listen(3000)
